4f0427585a66775f8998d6a17f4413299f2fd2ea
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-09 17:45:41 -0800

    user model and dateformat

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// create the Users model using the Users Schema
+const User = model("User", UserSchema);
+
+// Export Users module
+module.exports = User;
