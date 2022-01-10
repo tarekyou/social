2a61f0a8d58d4072d4d50487a563fc33d5962819
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-09 19:00:36 -0800

    thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getAllThought);
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:userId").post(createThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
