b8feeba6b199def7f04e4613c36331662dec857d
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-12 07:15:03 -0800

    delete thoughts upon deleting their owner

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# social
+# Social
+
+## Purpose
+
+A backend routes app to create user and have user write thoughts.
+
+## Built With
+
+- express
+- mongoose
+- Javascript
+
+## Walk-through video
+
+## Contribution
+
+Made with ❤️ by Tarek
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,6 +25,11 @@ const userController = {
           return;
         }
         // console.log("heristheuser" + dbUserData.thoughts);
+        // console.log(dbUserData);
+        // console.log(dbUserData.thoughts[0]._id);
+        for (let i = 0; i < dbUserData.thoughts.length; i++) {
+          console.log(dbUserData.thoughts[i]._id);
+        }
         res.json(dbUserData);
       })
       .catch((err) => {
@@ -66,21 +71,38 @@ const userController = {
   //       .catch((err) => res.status(400).json(err));
   //   },
   deleteUser({ params }, res) {
-    User.findOneAndDelete(
-      { _id: params.id }
-      //   { $pull: { thoughts: { _id: params.userId } } }
-    )
-      // .then(Thought.deleteMany().where((userId = params.id)))
+    User.findOneAndDelete({ _id: params.id })
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "No User with this particular ID!" });
           return;
         }
-        Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-        res.json(dbUserData);
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then((dbThoughtdata) => {
+        res.json(dbThoughtdata);
       })
       .catch((err) => res.status(400).json(err));
   },
+  //   deleteUser({ params }, res) {
+  //     User.findById({ _id: params.id })
+  //       .then((dbUserData) => {
+  //         Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+  //       })
+  //       .then((dbUserData) => {
+  //         User.findOneAndDelete({ _id: params.id });
+  //       })
+  //       .then((dbUserData) => {
+  //         if (!dbUserData) {
+  //           res.status(404).json({ message: "No User with this particular ID!" });
+  //           return;
+  //         }
+  //         // Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+  //         // Thought.deleteMany({ $in: dbUserData.thoughts });
+  //         res.json(dbUserData);
+  //       })
+  //       .catch((err) => res.status(400).json(err));
+  //   },
 
   addFriend({ params }, res) {
     User.findOneAndUpdate(
